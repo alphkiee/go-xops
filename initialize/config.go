4fa7a2f5e0980bc033ad77e8efe4975f344778e4
jkuup <alphkiee@gmail.com>
2021-01-23 11:05:32 +0800

    初始化配置文件和数据库

---
+++ initialize/config.go
@@ -0,0 +1,67 @@
+package initialize
+
+import (
+	"bytes"
+	"fmt"
+	"go-xops/pkg/common"
+	"os"
+
+	"github.com/gobuffalo/packr/v2"
+	"github.com/spf13/viper"
+)
+
+const (
+	configBoxName = "xops-conf-box"
+	configType    = "yml"
+	// 配置文件目录, packr.Box基于当前包目录, 文件名需要写完整, 即使viper可以自动获取
+	configPath = "../conf"
+	devConfig  = "config-dev.yml"
+	testConfig = "config-test.yml"
+	prodConfig = "config-prod.yml"
+)
+
+// 初始化配置文件
+func InitConfig() {
+	// 使用packr将配置文件打包到二进制文件中, 如果以docker镜像方式运行将会非常舒服
+	common.ConfBox = packr.New(configBoxName, configPath)
+	// 获取实例(可创建多实例读取多个配置文件, 这里不做演示)
+	v := viper.New()
+
+	// 读取开发环境配置作为默认配置项
+	readConfig(v, devConfig)
+	// 将default中的配置全部以默认配置写入
+	settings := v.AllSettings()
+	for index, setting := range settings {
+		v.SetDefault(index, setting)
+	}
+	// 读取当前go运行环境变量
+	env := os.Getenv("GO_ENV")
+	configName := ""
+	if env == "test" {
+		configName = testConfig
+	} else if env == "prod" {
+		configName = prodConfig
+	}
+	if configName != "" {
+		// 读取不同环境中的差异部分
+		readConfig(v, configName)
+	}
+	// 转换为结构体
+	if err := v.Unmarshal(&common.Conf); err != nil {
+		panic(fmt.Sprintf("初始化配置文件失败: %v", err))
+	}
+
+	fmt.Println("初始化配置文件完成")
+}
+
+func readConfig(v *viper.Viper, configFile string) {
+	v.SetConfigType(configType)
+	config, err := common.ConfBox.Find(configFile)
+	if err != nil {
+		panic(fmt.Sprintf("初始化配置文件失败: %v", err))
+	}
+	// 加载配置
+	if err = v.ReadConfig(bytes.NewReader(config)); err != nil {
+		panic(fmt.Sprintf("初始化配置文件失败: %v", err))
+	}
+}
---
+++ initialize/mysql.go
@@ -0,0 +1,63 @@
+package initialize
+
+import (
+	"fmt"
+	"go-xops/models/cmdb"
+	"go-xops/models/system"
+	"go-xops/pkg/common"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// 初始化mysql数据库
+func Mysql() {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?%s&charset=%s&collation=%s",
+		common.Conf.Mysql.Username,
+		common.Conf.Mysql.Password,
+		common.Conf.Mysql.Host,
+		common.Conf.Mysql.Port,
+		common.Conf.Mysql.Database,
+		common.Conf.Mysql.Query,
+		common.Conf.Mysql.Charset,
+		common.Conf.Mysql.Collation,
+	)
+	if common.Conf.Mysql.LogMode {
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+			// 禁用外键(指定外键时不会在mysql创建真实的外键约束)
+			DisableForeignKeyConstraintWhenMigrating: true,
+		})
+		if err != nil {
+			panic(fmt.Sprintf("初始化mysql异常: %v", err))
+		}
+		common.Mysql = db
+	} else {
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+			// 禁用外键(指定外键时不会在mysql创建真实的外键约束)
+			DisableForeignKeyConstraintWhenMigrating: true,
+		})
+		if err != nil {
+			panic(fmt.Sprintf("初始化mysql异常: %v", err))
+		}
+		common.Mysql = db
+	}
+
+	// 表结构
+	autoMigrate()
+}
+
+// 自动迁移表结构
+func autoMigrate() {
+	common.Mysql.AutoMigrate(
+		//new(models.SysCasbin),
+		new(system.SysUser),
+		new(system.SysDept),
+		new(system.SysRole),
+		new(system.SysMenu),
+		new(system.SysDict),
+		new(system.SysApi),
+		new(system.SysOperLog),
+		new(cmdb.Host),
+	)
+}
