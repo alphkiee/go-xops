8230ee81f77961b6f5739104e3d88ad825c83ffa
jkuup <alphkiee@gmail.com>
2021-01-26 09:18:23 +0800

    operlog

---
+++ routers/system/sys_operlog.go
@@ -0,0 +1,19 @@
+package system
+
+import (
+	"go-xops/api/v1/system"
+	"go-xops/middleware"
+
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+)
+
+// 菜单路由
+func InitOperLogRouter(r *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) (R gin.IRoutes) {
+	router := r.Group("operlog").Use(authMiddleware.MiddlewareFunc()).Use(middleware.CasbinMiddleware)
+	{
+		router.GET("/list", system.GetOperLogs)
+		router.DELETE("/delete", system.BatchDeleteOperLogByIds)
+	}
+	return router
+}
