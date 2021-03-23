c4d2ccc9421ae88ef222dff29851e49d66597c35
jkuup <alphkiee@gmail.com>
2021-03-23 09:21:29 +0800

    k8s api deployment

--- dto/request/k8s_request/application.go
+++ dto/request/k8s_request/application.go
@@ -6,3 +6,22 @@ type ApplicationReq struct {
 	Name      string `json:"name"`
 	Format    string `json:"format"`
 }
+
+type DeploymentReq struct {
+	DeployName    string            `json:"deployment"`
+	Replicas      int32             `json:"replicas"`
+	MatchLabels   map[string]string `json:"matchlabels"`
+	Labels        map[string]string `json:"labels"`
+	ImageName     string            `json:"imagename"`
+	Image         string            `json:"Image"`
+	NameSpace     string            `json:"namespace"`
+	ContainerPort uint              `json:"containerport"`
+}
+
+type UpDeploymentReq struct {
+	Replicas       int32  `json:"replicas"`
+	NameSpace      string `json:"namespace"`
+	DeploymentName string `json:"deploymentname"`
+	Image          string `json:"image"`
+	ContainerPort  uint   `json:"containerport"`
+}
