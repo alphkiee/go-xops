a6fa0e0560b3c533f0c7d3537a2d22d5361e699c
jkuup <alphkiee@gmail.com>
2021-03-24 09:35:10 +0800

    main

--- main.go
+++ main.go
@@ -39,22 +39,6 @@ func main() {
 	initialize.Casbin()
 	// 初始化创建上传目录
 	_ = os.MkdirAll(common.Conf.Upload.SaveDir+"/avatar/", 644)
-	//redis := cache.NewStringOperation()
-	//fmt.Println(redis.Set("b","xx"))
-
-	//是否初始化数据(慎用) $go-xops init
-	/*
-		if len(os.Args) > 1 {
-			if os.Args[1] == "init" {
-				initialize.InitData()
-				fmt.Println("数据初始化成功!")
-				os.Exit(1)
-			}
-		}
-	*/
-
-	// 关闭cache连接池
-	// defer common.Redis.Close()
 
 	// 启动服务器
 	host := "0.0.0.0"
@@ -65,29 +49,20 @@ func main() {
 	}
 
 	go func() {
-		// 加入pprof性能分析
 		if err := http.ListenAndServe(":8005", nil); err != nil {
 		}
 	}()
 
-	// Initializing the server in a goroutine so that
-	// it won't block the graceful shutdown handling below
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		}
 	}()
 
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
 	quit := make(chan os.Signal)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
